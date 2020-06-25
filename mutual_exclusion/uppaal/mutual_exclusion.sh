934f03ef64e157ce16fa7ae421317bbbc9aa8507
slarrozejar <sarah.larrozej@gmail.com>
2020-06-25 08:56:41 +0200

    include second version into scripts

--- mutual_exclusion/uppaal/mutual_exclusion.sh
+++ mutual_exclusion/uppaal/mutual_exclusion.sh
@@ -9,17 +9,25 @@
 function usage() {
     echo "Usage: $0 ";
     echo "       $0 R1 R2";
+    echo "       $0 N R1 R2"
     echo "       R1 request root 1, should be 0 or 1";
     echo "       R2 request root 2, should be 0 or 1";
-    echo "       default: R1=0, R2=0"
+    echo "       N set to 1 activates second mode of mutual exclusion"
+    echo "       default: N=0, R1=0, R2=0"
 }
 
 if [ $# -eq 2 ]; then
+    N=0
     R[1]=$1
     R[2]=$2
 elif [ $# -eq 0 ]; then
+    N=0
     R[1]=0
     R[2]=0
+elif [ $# -eq 3 ]; then
+    N=$1
+    R[1]=$2
+    R[2]=$3
 else
     usage
     exit 1
@@ -36,9 +44,13 @@ echo "<?xml version='1.0' encoding='utf-8'?>
 echo "	<declaration>
 //Inspired from UPPAAL demo model introduced in Section 5 in:
 //Martin Wehrle, Sebastian Kupferschmid:
-//Mcta: Heuristics and Search for Timed Systems. FORMATS 2012: 252-266
-
-clock S1_z;
+//Mcta: Heuristics and Search for Timed Systems. FORMATS 2012: 252-266"
+if [ $N -eq 1 ]; then
+    echo "clock S1_x;
+clock S2_x;
+clock Ctrl_x;"
+fi
+echo "clock S1_z;
 clock S2_z;
 clock Ctrl_z;
 clock Ctrl_y0;
@@ -66,6 +78,15 @@ echo "</declaration>"
 ID=0
 
 for i in `seq 1 2`; do
+if [ $N -eq 1 ]; then
+    cond="&amp;&amp;S${i}_x &gt; 0 &amp;&amp;S${i}_z &lt; 1000"
+    aff1=", S${i}_x := 0"
+    aff2=", S${i}_x := 0, Ctrl_x := 0"
+else
+    cond=""
+    aff1=""
+    aff2=""
+fi
     echo "<template>
 <name>S$i</name>"
     echo "<location id='id$ID'>
@@ -97,7 +118,7 @@ for i in `seq 1 2`; do
 <source ref='id$(($ID+1))'/>
 <target ref='id$(($ID+1))'/>
 <label kind='guard'>S${i}_IntStat == 1 &amp;&amp; polled_S${i}_request == 0 &amp;&amp; polled_S${i}_grant == 1 &amp;&amp; S${i}_Safe == 1</label>
-<label kind='assignment'>S${i}_IntStat := 0, S${i}_z := 0</label>
+<label kind='assignment'>S${i}_IntStat := 0, S${i}_z := 0$aff1</label>
 </transition>"
     fi
     echo "<transition>
@@ -109,13 +130,13 @@ for i in `seq 1 2`; do
 <source ref='id$(($ID+1))'/>
 <target ref='id$(($ID+1))'/>
 <label kind='guard'>S${i}_IntStat == 2</label>
-<label kind='assignment'>S${i}_IntStat := 0, S${i}_z := 0</label>
+<label kind='assignment'>S${i}_IntStat := 0, S${i}_z := 0$aff1</label>
 </transition>
 <transition>
 <source ref='id$(($ID+2))'/>
 <target ref='id$(($ID+2))'/>
 <label kind='guard'>S${i}_IntStat == 2</label>
-<label kind='assignment'>S${i}_IntStat := 0, S${i}_z := 0</label>
+<label kind='assignment'>S${i}_IntStat := 0, S${i}_z := 0$aff1</label>
 </transition>"
     if [ ${R[$i]} -eq 1 ]; then
         add=", polled_S${i}_request := Root_req$i"
@@ -125,14 +146,14 @@ for i in `seq 1 2`; do
     echo "<transition>
 <source ref='id$(($ID+1))'/>
 <target ref='id$(($ID+1))'/>
-<label kind='guard'>S${i}_IntStat == 0 &amp;&amp; S${i}_z &gt; 0</label>
-<label kind='assignment'>polled_S${i}_grant := Ctrl_grant$i, S${i}_IntStat := 1$add</label>
+<label kind='guard'>S${i}_IntStat == 0 &amp;&amp; S${i}_z &gt; 0 $cond</label>
+<label kind='assignment'>polled_S${i}_grant := Ctrl_grant$i, S${i}_IntStat := 1$add $aff1</label>
 </transition>
 <transition>
 <source ref='id$(($ID+2))'/>
 <target ref='id$(($ID+2))'/>
-<label kind='guard'>S${i}_IntStat == 0 &amp;&amp; S${i}_z &gt; 0</label>
-<label kind='assignment'>polled_S${i}_grant := Ctrl_grant$i, S${i}_IntStat := 1$add</label>
+<label kind='guard'>S${i}_IntStat == 0 &amp;&amp; S${i}_z &gt; 0 $cond</label>
+<label kind='assignment'>polled_S${i}_grant := Ctrl_grant$i, S${i}_IntStat := 1$add $aff1</label>
 </transition>"
     if [ ${R[$i]} -eq 1 ]; then
         add="&amp;&amp; polled_S${i}_request == 0"
@@ -143,13 +164,13 @@ for i in `seq 1 2`; do
 <source ref='id$(($ID+1))'/>
 <target ref='id$(($ID+1))'/>
 <label kind='guard'>S${i}_IntStat == 1 &amp;&amp; polled_S${i}_grant == 0 &amp;&amp; S${i}_Safe == 1$add</label>
-<label kind='assignment'>S${i}_IntStat := 0, S${i}_z := 0</label>
+<label kind='assignment'>S${i}_IntStat := 0, S${i}_z := 0 $aff1</label>
 </transition>
 <transition>
 <source ref='id$(($ID+2))'/>
 <target ref='id$(($ID+1))'/>
 <label kind='guard'>S${i}_IntStat == 1 &amp;&amp; S${i}_Safe == 0$add</label>
-<label kind='assignment'>S${i}_Safe := 1, S${i}_IntStat := 0, S${i}_z := 0</label>
+<label kind='assignment'>S${i}_Safe := 1, S${i}_IntStat := 0, S${i}_z := 0 $aff2</label>
 </transition>"
     if [ ${R[$i]} -eq 1 ]; then
         add="&amp;&amp; polled_S${i}_request == 1"
@@ -160,13 +181,13 @@ for i in `seq 1 2`; do
 <source ref='id$(($ID+2))'/>
 <target ref='id$(($ID+2))'/>
 <label kind='guard'>S${i}_IntStat == 1 &amp;&amp; S${i}_Safe == 0$add</label>
-<label kind='assignment'>S${i}_IntStat := 0, S${i}_z := 0</label>
+<label kind='assignment'>S${i}_IntStat := 0, S${i}_z := 0 $aff1</label>
 </transition>
 <transition>
 <source ref='id$(($ID+1))'/>
 <target ref='id$(($ID+2))'/>
 <label kind='guard'>S${i}_IntStat == 1 &amp;&amp; polled_S${i}_grant == 1 &amp;&amp; S${i}_Safe == 1$add</label>
-<label kind='assignment'>S${i}_Safe := 0, S${i}_IntStat := 0, S${i}_z := 0</label>
+<label kind='assignment'>S${i}_Safe := 0, S${i}_IntStat := 0, S${i}_z := 0 $aff2</label>
 </transition>"
     if [ ${R[$i]} -eq 1 ]; then
         add="polled_S${i}_request == 1 &amp;&amp; polled_S${i}_grant == 0"
@@ -177,7 +198,7 @@ for i in `seq 1 2`; do
 <source ref='id$(($ID+1))'/>
 <target ref='id$(($ID+1))'/>
 <label kind='guard'>S${i}_IntStat == 1 &amp;&amp; $add &amp;&amp; S${i}_Safe == 1</label>
-<label kind='assignment'>S${i}_IntStat := 0, S${i}_z := 0</label>
+<label kind='assignment'>S${i}_IntStat := 0, S${i}_z := 0 $aff1</label>
 </transition>"
     echo "</template>"
 
@@ -234,29 +255,11 @@ echo "<template>
 <name>g2</name>
 <label kind='invariant'>Ctrl_z &lt;= 1000</label>
 </location>
-<init ref='id$ID'/>
-<transition>
+<init ref='id$ID'/>"
+echo "<transition>
 <source ref='id$ID'/>
 <target ref='id$(($ID+1))'/>
 <label kind='assignment'>Ctrl_grant1 := 0, Ctrl_grant2 := 0, Ctrl_IntStat := 0</label>
-</transition>
-<transition>
-<source ref='id$(($ID+1))'/>
-<target ref='id$(($ID+1))'/>
-<label kind='guard'>Ctrl_IntStat == 0 &amp;&amp; Ctrl_z &gt; 0</label>
-<label kind='assignment'>polled_Ctrl_Safe1 := S1_Safe, polled_Ctrl_Safe2 := S2_Safe, Ctrl_IntStat := 1</label>
-</transition>
-<transition>
-<source ref='id$(($ID+1))'/>
-<target ref='id$(($ID+1))'/>
-<label kind='guard'>Ctrl_IntStat == 2</label>
-<label kind='assignment'>Ctrl_IntStat := 0, Ctrl_z := 0</label>
-</transition>
-<transition>
-<source ref='id$(($ID+1))'/>
-<target ref='id$(($ID+1))'/>
-<label kind='guard'>Ctrl_IntStat == 1 &amp;&amp; polled_Ctrl_Safe2 == 0 &amp;&amp; Ctrl_grant1 == 0</label>
-<label kind='assignment'>Ctrl_IntStat := 0, Ctrl_z := 0</label>
 </transition>
 <transition>
 <source ref='id$(($ID+1))'/>
@@ -265,58 +268,10 @@ echo "<template>
 <label kind='assignment'>Ctrl_IntStat := 2</label>
 </transition>
 <transition>
-<source ref='id$(($ID+1))'/>
-<target ref='id$(($ID+2))'/>
-<label kind='guard'>Ctrl_IntStat == 1 &amp;&amp; polled_Ctrl_Safe2 == 1 &amp;&amp; Ctrl_grant1 == 0 &amp;&amp; Ctrl_y0 &gt;= 2000</label>
-<label kind='assignment'>Ctrl_grant1 := 1, Ctrl_IntStat := 0, Ctrl_z := 0, Ctrl_y0 := 0</label>
-</transition>
-<transition>
-<source ref='id$(($ID+2))'/>
-<target ref='id$(($ID+2))'/>
-<label kind='guard'>Ctrl_IntStat == 0 &amp;&amp; Ctrl_z &gt; 0</label>
-<label kind='assignment'>polled_Ctrl_Safe1 := S1_Safe, polled_Ctrl_Safe2 := S2_Safe, Ctrl_IntStat := 1</label>
-</transition>
-<transition>
-<source ref='id$(($ID+2))'/>
-<target ref='id$(($ID+2))'/>
-<label kind='guard'>Ctrl_IntStat == 2</label>
-<label kind='assignment'>Ctrl_IntStat := 0, Ctrl_z := 0</label>
-</transition>
-<transition>
-<source ref='id$(($ID+2))'/>
-<target ref='id$(($ID+3))'/>
-<label kind='guard'>Ctrl_IntStat == 1 &amp;&amp; polled_Ctrl_Safe1 == 0 &amp;&amp; Ctrl_grant1 == 1</label>
-<label kind='assignment'>Ctrl_grant1 := 0, Ctrl_IntStat := 0, Ctrl_z := 0, Ctrl_y0 := 0</label>
-</transition>
-<transition>
 <source ref='id$(($ID+2))'/>
 <target ref='id$(($ID+2))'/>
 <label kind='guard'>Ctrl_IntStat == 1 &amp;&amp; polled_Ctrl_Safe1 == 1 &amp;&amp; Ctrl_grant1 == 1 &amp;&amp; Ctrl_y0 &lt; 2000</label>
 <label kind='assignment'>Ctrl_IntStat := 2</label>
-</transition>
-<transition>
-<source ref='id$(($ID+2))'/>
-<target ref='id$(($ID+3))'/>
-<label kind='guard'>Ctrl_IntStat == 1 &amp;&amp; polled_Ctrl_Safe1 == 1 &amp;&amp; Ctrl_grant1 == 1 &amp;&amp; Ctrl_y0 &gt;= 2000</label>
-<label kind='assignment'>Ctrl_grant1 := 0, Ctrl_IntStat := 0, Ctrl_z := 0, Ctrl_y0 := 0</label>
-</transition>
-<transition>
-<source ref='id$(($ID+3))'/>
-<target ref='id$(($ID+3))'/>
-<label kind='guard'>Ctrl_IntStat == 0 &amp;&amp; Ctrl_z &gt; 0</label>
-<label kind='assignment'>polled_Ctrl_Safe1 := S1_Safe, polled_Ctrl_Safe2 := S2_Safe, Ctrl_IntStat := 1</label>
-</transition>
-<transition>
-<source ref='id$(($ID+3))'/>
-<target ref='id$(($ID+3))'/>
-<label kind='guard'>Ctrl_IntStat == 2</label>
-<label kind='assignment'>Ctrl_IntStat := 0, Ctrl_z := 0</label>
-</transition>
-<transition>
-<source ref='id$(($ID+3))'/>
-<target ref='id$(($ID+3))'/>
-<label kind='guard'>Ctrl_IntStat == 1 &amp;&amp; polled_Ctrl_Safe1 == 0 &amp;&amp; Ctrl_grant2 == 0</label>
-<label kind='assignment'>Ctrl_IntStat := 0, Ctrl_z := 0</label>
 </transition>
 <transition>
 <source ref='id$(($ID+3))'/>
@@ -325,42 +280,125 @@ echo "<template>
 <label kind='assignment'>Ctrl_IntStat := 2</label>
 </transition>
 <transition>
-<source ref='id$(($ID+3))'/>
-<target ref='id$(($ID+4))'/>
-<label kind='guard'>Ctrl_IntStat == 1 &amp;&amp; polled_Ctrl_Safe1 == 1 &amp;&amp; Ctrl_grant2 == 0 &amp;&amp; Ctrl_y0 &gt;= 2000</label>
-<label kind='assignment'>Ctrl_grant2 := 1, Ctrl_IntStat := 0, Ctrl_z := 0, Ctrl_y0 := 0</label>
-</transition>
-<transition>
 <source ref='id$(($ID+4))'/>
 <target ref='id$(($ID+4))'/>
-<label kind='guard'>Ctrl_IntStat == 0 &amp;&amp; Ctrl_z &gt; 0</label>
-<label kind='assignment'>polled_Ctrl_Safe1 := S1_Safe, polled_Ctrl_Safe2 := S2_Safe, Ctrl_IntStat := 1</label>
+<label kind='guard'>Ctrl_IntStat == 1 &amp;&amp; polled_Ctrl_Safe2 == 1 &amp;&amp; Ctrl_grant2 == 1 &amp;&amp; Ctrl_y0 &lt; 2000</label>
+<label kind='assignment'>Ctrl_IntStat := 2</label>
+</transition>"
+if [ $N -eq 1 ]; then
+    aff=", Ctrl_x := 0"
+else
+    aff=""
+fi
+echo "<transition>
+<source ref='id$(($ID+1))'/>
+<target ref='id$(($ID+1))'/>
+<label kind='guard'>Ctrl_IntStat == 2</label>
+<label kind='assignment'>Ctrl_IntStat := 0, Ctrl_z := 0 $aff</label>
+</transition>
+<transition>
+<source ref='id$(($ID+1))'/>
+<target ref='id$(($ID+1))'/>
+<label kind='guard'>Ctrl_IntStat == 1 &amp;&amp; polled_Ctrl_Safe2 == 0 &amp;&amp; Ctrl_grant1 == 0</label>
+<label kind='assignment'>Ctrl_IntStat := 0, Ctrl_z := 0 $aff</label>
+</transition>
+<transition>
+<source ref='id$(($ID+2))'/>
+<target ref='id$(($ID+2))'/>
+<label kind='guard'>Ctrl_IntStat == 2</label>
+<label kind='assignment'>Ctrl_IntStat := 0, Ctrl_z := 0 $aff</label>
+</transition>
+<transition>
+<source ref='id$(($ID+3))'/>
+<target ref='id$(($ID+3))'/>
+<label kind='guard'>Ctrl_IntStat == 2</label>
+<label kind='assignment'>Ctrl_IntStat := 0, Ctrl_z := 0 $aff</label>
+</transition>
+<transition>
+<source ref='id$(($ID+3))'/>
+<target ref='id$(($ID+3))'/>
+<label kind='guard'>Ctrl_IntStat == 1 &amp;&amp; polled_Ctrl_Safe1 == 0 &amp;&amp; Ctrl_grant2 == 0</label>
+<label kind='assignment'>Ctrl_IntStat := 0, Ctrl_z := 0 $aff</label>
 </transition>
 <transition>
 <source ref='id$(($ID+4))'/>
 <target ref='id$(($ID+4))'/>
 <label kind='guard'>Ctrl_IntStat == 2</label>
-<label kind='assignment'>Ctrl_IntStat := 0, Ctrl_z := 0</label>
+<label kind='assignment'>Ctrl_IntStat := 0, Ctrl_z := 0 $aff</label>
+</transition>"
+if [ $N -eq 1 ]; then
+    cond="&amp;&amp; Ctrl_x &gt; 0 &amp;&amp; Ctrl_z &lt; 1000"
+else
+    cond=""
+fi
+echo "<transition>
+<source ref='id$(($ID+1))'/>
+<target ref='id$(($ID+1))'/>
+<label kind='guard'>Ctrl_IntStat == 0 &amp;&amp; Ctrl_z &gt; 0 $cond</label>
+<label kind='assignment'>polled_Ctrl_Safe1 := S1_Safe, polled_Ctrl_Safe2 := S2_Safe, Ctrl_IntStat := 1 $aff</label>
+</transition>
+<transition>
+<source ref='id$(($ID+2))'/>
+<target ref='id$(($ID+2))'/>
+<label kind='guard'>Ctrl_IntStat == 0 &amp;&amp; Ctrl_z &gt; 0 $cond</label>
+<label kind='assignment'>polled_Ctrl_Safe1 := S1_Safe, polled_Ctrl_Safe2 := S2_Safe, Ctrl_IntStat := 1 $aff</label>
+</transition>
+<transition>
+<source ref='id$(($ID+3))'/>
+<target ref='id$(($ID+3))'/>
+<label kind='guard'>Ctrl_IntStat == 0 &amp;&amp; Ctrl_z &gt; 0 $cond</label>
+<label kind='assignment'>polled_Ctrl_Safe1 := S1_Safe, polled_Ctrl_Safe2 := S2_Safe, Ctrl_IntStat := 1 $aff</label>
+</transition>
+<transition>
+<source ref='id$(($ID+4))'/>
+<target ref='id$(($ID+4))'/>
+<label kind='guard'>Ctrl_IntStat == 0 &amp;&amp; Ctrl_z &gt; 0 $cond</label>
+<label kind='assignment'>polled_Ctrl_Safe1 := S1_Safe, polled_Ctrl_Safe2 := S2_Safe, Ctrl_IntStat := 1 $aff</label>
+</transition>"
+if [ $N -eq 1 ]; then
+    aff=", Ctrl_x := 0, S1_x := 0, S2_x := 0"
+else
+    aff=""
+fi
+echo "<transition>
+<source ref='id$(($ID+1))'/>
+<target ref='id$(($ID+2))'/>
+<label kind='guard'>Ctrl_IntStat == 1 &amp;&amp; polled_Ctrl_Safe2 == 1 &amp;&amp; Ctrl_grant1 == 0 &amp;&amp; Ctrl_y0 &gt;= 2000</label>
+<label kind='assignment'>Ctrl_grant1 := 1, Ctrl_IntStat := 0, Ctrl_z := 0, Ctrl_y0 := 0 $aff</label>
+</transition>
+<transition>
+<source ref='id$(($ID+2))'/>
+<target ref='id$(($ID+3))'/>
+<label kind='guard'>Ctrl_IntStat == 1 &amp;&amp; polled_Ctrl_Safe1 == 0 &amp;&amp; Ctrl_grant1 == 1</label>
+<label kind='assignment'>Ctrl_grant1 := 0, Ctrl_IntStat := 0, Ctrl_z := 0, Ctrl_y0 := 0 $aff</label>
+</transition>
+<transition>
+<source ref='id$(($ID+2))'/>
+<target ref='id$(($ID+3))'/>
+<label kind='guard'>Ctrl_IntStat == 1 &amp;&amp; polled_Ctrl_Safe1 == 1 &amp;&amp; Ctrl_grant1 == 1 &amp;&amp; Ctrl_y0 &gt;= 2000</label>
+<label kind='assignment'>Ctrl_grant1 := 0, Ctrl_IntStat := 0, Ctrl_z := 0, Ctrl_y0 := 0 $aff</label>
+</transition>
+<transition>
+<source ref='id$(($ID+3))'/>
+<target ref='id$(($ID+4))'/>
+<label kind='guard'>Ctrl_IntStat == 1 &amp;&amp; polled_Ctrl_Safe1 == 1 &amp;&amp; Ctrl_grant2 == 0 &amp;&amp; Ctrl_y0 &gt;= 2000</label>
+<label kind='assignment'>Ctrl_grant2 := 1, Ctrl_IntStat := 0, Ctrl_z := 0, Ctrl_y0 := 0 $aff</label>
 </transition>
 <transition>
 <source ref='id$(($ID+4))'/>
 <target ref='id$(($ID+1))'/>
 <label kind='guard'>Ctrl_IntStat == 1 &amp;&amp; polled_Ctrl_Safe2 == 0 &amp;&amp; Ctrl_grant2 == 1</label>
-<label kind='assignment'>Ctrl_grant2 := 0, Ctrl_IntStat := 0, Ctrl_z := 0, Ctrl_y0 := 0</label>
-</transition>
-<transition>
-<source ref='id$(($ID+4))'/>
-<target ref='id$(($ID+4))'/>
-<label kind='guard'>Ctrl_IntStat == 1 &amp;&amp; polled_Ctrl_Safe2 == 1 &amp;&amp; Ctrl_grant2 == 1 &amp;&amp; Ctrl_y0 &lt; 2000</label>
-<label kind='assignment'>Ctrl_IntStat := 2</label>
+<label kind='assignment'>Ctrl_grant2 := 0, Ctrl_IntStat := 0, Ctrl_z := 0, Ctrl_y0 := 0 $aff</label>
 </transition>
 <transition>
 <source ref='id$(($ID+4))'/>
 <target ref='id$(($ID+1))'/>
 <label kind='guard'>Ctrl_IntStat == 1 &amp;&amp; polled_Ctrl_Safe2 == 1 &amp;&amp; Ctrl_grant2 == 1 &amp;&amp; Ctrl_y0 &gt;= 2000</label>
-<label kind='assignment'>Ctrl_grant2 := 0, Ctrl_IntStat := 0, Ctrl_z := 0, Ctrl_y0 := 0</label>
+<label kind='assignment'>Ctrl_grant2 := 0, Ctrl_IntStat := 0, Ctrl_z := 0, Ctrl_y0 := 0 $aff</label>
 </transition>
 </template>"
+
+
 
 
 if [ ${R[1]} -eq 1 ]; then

a2f6d436b92e31dfcbc7c752f3c6382ec65275de
slarrozejar <sarah.larrozej@gmail.com>
2020-07-01 15:55:51 +0200

    add some tests

--- program/test_dot2dot.py
+++ program/test_dot2dot.py
@@ -1,5 +1,4 @@
 #! /usr/bin/python3
-
 import pygraphviz as pgv
 import sys
 import os
---
+++ program/test_dotStyle.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+import pygraphviz as pgv
+import sys
+import os
+
+def test_nochange():
+    os.system('./dotStyle.py graph.dot > test.dot 2>/dev/null')
+    test = pgv.AGraph("test.dot")
+    result = pgv.AGraph("graph.dot")
+    if(test == result):
+        print("\033[32m" + "Test no change: Passed")
+    else:
+        print("Test no change: Failed")
+
+def test_line_node():
+    os.system('./dotStyle.py graph.dot -sn b=3&&a=4 color=yellow > test.dot 2>/dev/null')
+    test = pgv.AGraph("test.dot")
+    result = pgv.AGraph("test_line_node.dot")
+    if(test == result):
+        print("\033[32m" + "Test line node: Passed")
+    else:
+        print("Test line node: Failed")
+
+test_nochange()
+test_line_node()

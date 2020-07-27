d244b5fa62231fdfe3d66379bb2c8401a051bc45
slarrozejar <sarah.larrozej@gmail.com>
2020-07-27 09:52:32 +0200

    try to display csv file

---
+++ web/script.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const papa = require('papaparse');
+const file = fs.createReadStream('../models.csv');
+var count = 0; // cache the running count
+papa.parse(file, {
+    worker: true, // Don't bog down the main thread if its a big file
+    step: function(result) {
+        // do stuff with result
+    },
+    complete: function(results, file) {
+        console.log('parsing complete read', count, 'records.');
+    }
+});
